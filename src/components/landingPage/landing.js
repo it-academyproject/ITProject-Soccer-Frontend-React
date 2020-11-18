7e89b90d314d440295bfc87d64d0e915d3a2ad9f
Marc Ec <espanolmarc@gmail.com>
2020-11-18 12:57:24 +0100

    Form Validation

--- src/components/landingPage/landing.js
+++ src/components/landingPage/landing.js
@@ -1,39 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Button, Container, Row, Col, Form } from 'react-bootstrap';
 
 import './landing.css';
 
-function Landing() {
-    return (
-      <div className="landing">
-        <Container>
-            <Row style={{width: '100%'}}>
-                <Col xs={12} md={6}>
-                </Col>
-                <Col xs={12} md={6} style={{height: '100vh', width: '100%'}}>
-                    <Form className="loginForm">
-                        <Form.Group>
-                            <Form.Label className="formLabel">username</Form.Label>
-                            <Form.Control type="text" placeholder="enter your username" className="formControl" />
-                        </Form.Group>
+import FormErrors from './FormErrors.js';
 
-                        <Form.Group>
-                            <Form.Label className="formLabel">password</Form.Label>
-                            <Form.Control type="password" placeholder="password" className="formControl" />
-                            <Form.Text className="text-muted">
-                                <a href="#">you forgot the password?</a>
-                            </Form.Text>
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Sign in
-                        </Button>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
-      </div>
-    );
+class Landing extends Component {
+        constructor (props) {
+            super(props);
+            this.state = {
+              username: '',
+              password: '',
+              formErrors: {username: '', password: ''},
+              usernameValid: false,
+              passwordValid: false,
+              formValid: false
+            }
+        }
+        
+        handleChange = (e) => {
+            const name = e.target.name;
+            const value = e.target.value;
+            this.setState(
+                {[name]: value},
+                () => { this.validateField(name, value)}
+            );
+        }
+
+        validateField(fieldName, value) {
+            let fieldValidationErrors = this.state.formErrors;
+            let usernameValid = this.state.usernameValid;
+            let passwordValid = this.state.passwordValid;
+          
+            switch(fieldName) {
+              case 'username':
+                usernameValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+                fieldValidationErrors.username = usernameValid ? '' : ' is invalid';
+                break;
+              case 'password':
+                passwordValid = value.length >= 8;
+                fieldValidationErrors.password = passwordValid ? '': ' is too short';
+                break;
+              default:
+                break;
+            }
+
+            this.setState({formErrors: fieldValidationErrors,
+                            usernameValid: usernameValid,
+                            passwordValid: passwordValid
+                          }, this.validateForm);
+        }
+          
+          validateForm() {
+            this.setState({formValid: this.state.usernameValid && this.state.passwordValid});
+          }
+
+        render () {
+            return (
+                <div className="landing">
+                <Container>
+                    <Row style={{width: '100%'}}>
+                        <Col xs={12} md={6}>
+                        </Col>
+                        <Col xs={12} md={6} style={{height: '100vh', width: '100%'}}>
+                            <Form className="loginForm">
+                                <Form.Group>
+                                    <Form.Label className="formLabel">username</Form.Label>
+                                    <Form.Control type="email" required placeholder="enter your username" name="username" className="formControl" value={this.state.username} onChange={(event) => this.handleChange(event)}/>
+                                </Form.Group>
+        
+                                <Form.Group>
+                                    <Form.Label className="formLabel">password</Form.Label>
+                                    <Form.Control as="input" type="password" required placeholder="password" name="password" className="formControl" value={this.state.password} onChange={(event) => this.handleChange(event)}/>
+                                    <Form.Text className="text-muted">
+                                        <a href="#">you forgot the password?</a>
+                                    </Form.Text>
+                                </Form.Group>
+                                <Button variant="primary" type="submit" disabled={!this.state.formValid}>
+                                    Sign in
+                                </Button>
+                                <div className="panel panel-default">
+                                    <FormErrors formErrors={this.state.formErrors} />
+                                </div>
+                            </Form>
+                        </Col>
+                    </Row>    
+                </Container>
+                </div>
+            );
+        }
   }
 
   export default Landing;
