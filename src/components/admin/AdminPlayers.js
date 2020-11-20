df3117f2ee01c76abc2c369c52a3a457dece196b
jc_projects <Juan Carlos>
2020-11-20 12:12:15 +0100

    Crear layout AdminPlayers (Table Button)

--- src/components/admin/AdminPlayers.js
+++ src/components/admin/AdminPlayers.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import Image from 'react-bootstrap/Image';
+import { Row, Col, Table, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import image from '../img/Admin-Players.png';
 
 const TOKEN = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE2MDU4NjAyOTAsImlzcyI6InNlY3VyZS1hcGkiLCJzdWIiOiJyYWxjYWxkZUBzb2NjZXIuY29tIiwiZXhwIjoxNjA2NzA2MjkwfQ.bKiFRTF_6DakyMs25aE5untDfUFbVdldeAB7qDeYVrGQzNGoaByfELBu-m9rFITl1_B_ulC6VuE26P2FT_nqpw';
 const API = 'http://217.76.158.200:8181/api';
@@ -44,18 +43,71 @@ class AdminPlayers extends Component {
         console.log(this.state.dataPlayers);
         console.log(this.state.dataTeams);
     }
-
-
     render() {
         return (
-            <Link to="/adminPlayers/Single" >
-                <Image src={image} fluid />
-            </Link>
-
+            <Row className="d-flex justify-content-center">
+                <Col xs="8">
+                    <Row>
+                        <Col xs="10" >
+                            <Table striped bordered hover>
+                                <thead className="thead-dark ">
+                                    <tr>
+                                        <th>Player name</th>
+                                        <th>Team</th>
+                                        <th>Options</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>
+                                           <a href="#">Delete</a> ------- <a href="#">View Detail</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Col>
+                        <Col xs="2">
+                            <Link to="/adminPlayers/Single" >
+                                <Button variant="warning" size="lg">New Player</Button>
+                            </Link>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
         );
-
     }
-
 }
 
 export default AdminPlayers;
