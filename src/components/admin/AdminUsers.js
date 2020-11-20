31902da3c60be4561b2732e46ae997ee09cbdf87
Marc Ec <espanolmarc@gmail.com>
2020-11-20 16:47:57 +0100

    Admin Users

--- src/components/admin/AdminUsers.js
+++ src/components/admin/AdminUsers.js
@@ -1,14 +1,67 @@
-import React from 'react';
-import Image from 'react-bootstrap/Image';
-import image from '../img/Admin-Managers.png';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Button, Container, Row, Col, Table } from 'react-bootstrap';
 
-const AdminUsers = () => {
-    return (
-        <Link to="/adminUsers/Single">
-            <Image src={image} fluid />
-        </Link>
-    );
+class AdminUsers extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataManagers: [],
+            id: "",
+            type_user: "",
+            email: "",
+            team_id: null,
+            team_name: null,
+            players: null, 
+        }
+
+    }
+
+    componentDidMount() {
+        fetch("http://217.76.158.200:8181/api/users", {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE2MDU4NjAyOTAsImlzcyI6InNlY3VyZS1hcGkiLCJzdWIiOiJyYWxjYWxkZUBzb2NjZXIuY29tIiwiZXhwIjoxNjA2NzA2MjkwfQ.bKiFRTF_6DakyMs25aE5untDfUFbVdldeAB7qDeYVrGQzNGoaByfELBu-m9rFITl1_B_ulC6VuE26P2FT_nqpw'
+            },
+        })
+            .then(response => response.json())
+            .then(data => this.setState({ dataManagers: data }));
+    }
+
+    render() {
+        return (
+                <Container>
+                    <Row>
+                        <Col xs={1}>
+                        </Col>
+                        <Col xs={10}>
+                        <Table className="table table-hover" style={{backgroundColor:'#fff', borderRadius: '5px'}}>
+                            <thead className="thead-light"> 
+                                <tr>
+                                    <th>Manager Name</th>
+                                    <th>Team</th>
+                                    <th>Options</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.state.dataManagers.map(manager =>
+                                <tr key={manager.id}>
+                                    <td>{manager.email}</td>
+                                    <td>{manager.password}</td>
+                                    <td><a href="#">Delete</a> / <a href="#">View Details</a></td>
+                                </tr>
+                                )}
+                            </tbody>
+                        </Table>
+                        <Button variant="warning" type="submit">New Manager</Button>
+                        </Col>
+                        <Col xs={1}>
+                        </Col>
+                    </Row>
+                </Container>
+        );
+    }
 }
 
 export default AdminUsers;
